3f611258d8a91f4420a1ba4c75b72647a4b28a3c
Kayode Adechinan <kayode.adechinan@gmail.com>
2021-05-16 18:04:27 +0000

    updates

--- api/index.ts
+++ api/index.ts
@@ -3,3 +3,5 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 export default (req: VercelRequest, res: VercelResponse) => {
   return res.json({ message: 'Hello World' });
 };
+
+// https://blog.logrocket.com/serverless-deployments-vercel-node-js/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-vercel';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .post('https://ng-vercel-peach.vercel.app/api', {
+        message: 'hello',
+      })
+      .subscribe((res) => {
+        console.log(res);
+
+        this.http
+          .get('https://ng-vercel-peach.vercel.app/api')
+          .subscribe((r) => {
+            console.log(r);
+          });
+      });
+  }
 }
